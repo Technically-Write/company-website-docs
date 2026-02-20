a512cad8d76fd3026e77df0ba8d7fe5896f94a80
colin hillis <colin.hillis@blackwalll.com>
2026-02-20 01:02:03 +0000

    setting site url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://www.technicallywrite.co.uk',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
