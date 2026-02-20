13f491f0cc998819aca4e5243041412a607f8e44
colin hillis <colin.hillis@blackwalll.com>
2026-02-20 20:32:05 +0000

    tab name 2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'WELCOME TO TECHNICALLY WRITE LTD.',
+  title: ' ',
   tagline: 'Clear technical documentation that simplifies complex products and helps your business to scale.',
   customFields: {
     heroDescription:
