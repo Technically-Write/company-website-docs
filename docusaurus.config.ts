5cf3c5ed1c980148330cfd7811317fa9608c0caa
colin hillis <colin.hillis@blackwalll.com>
2026-05-07 10:44:06 +0100

    build fix again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,9 +75,7 @@ const config: Config = {
         onInlineTags: 'warn',
         onInlineAuthors: 'warn',
         onUntruncatedBlogPosts: 'warn',
-          // 👇 ADDED THIS TO UN-INDEX AUTHORS AND TAGS
         archiveBasePath: null,
-        tagsBasePath: 'tags',
         authorsBasePath: null,
         // editUrl:
         //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
@@ -210,4 +208,3 @@ const config: Config = {
 };
 
 export default config;
-
